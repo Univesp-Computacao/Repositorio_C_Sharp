11c14abc3a90e4bd0b323c45f49ce159d0a098f5
Kalel Jamil Alves <73508900+kalelalves@users.noreply.github.com>
2022-04-01 12:51:16 -0300

    Adicionar arquivos de projeto.

---
+++ Repositorio_C_Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repositorio_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
